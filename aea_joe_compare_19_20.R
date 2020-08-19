18e0feb08ca8de9b38c97733bde4cc87b21dd1a1
jlvoorheis <69638370+jlvoorheis@users.noreply.github.com>
2020-08-19 19:47:30 -0400

    Update aea_joe_compare_19_20.R

--- aea_joe_compare_19_20.R
+++ aea_joe_compare_19_20.R
@@ -12,7 +12,7 @@ try19_df <- data.frame(date = as.Date(gsub("Date Posted: ", "", try1), format="%
   group_by(date) %>% summarise(n_ads = n()) %>% ungroup() %>% mutate(total_ads = cumsum(n_ads), day_in_year = yday(date), year = as.factor(year(date)), month=month(date))
 
 try19_df %>%
-ggplot() + 
+  ggplot() + 
   geom_line(aes(x=date, y=total_ads))
 
 #aea_20 <- read_html("aea_20.html")
@@ -29,16 +29,29 @@ all_aea %>%
   ggplot() + 
   geom_line(aes(x=day_in_year, y=total_ads, group=year, colour=year))+
   scale_colour_brewer(palette="Set1")+theme_bw()+
-  labs(title="Cumulative Number of JOE Ads, 2019 vs. 2020 to Date")
+  labs(title="Cumulative Number of JOE Ads, 2019 vs. 2020 to Date",
+       x="Day in Year (Aug 1 = 213)",
+       y="Cumulative Ads Posted on JOE")
 
 
 all_aea %>% 
   filter(month%in% c(8)) %>%
   arrange(day_in_year, year) %>%
-  group_by(day_in_year) %>% summarise(diff_ads = mean(total_ads - lag(total_ads), na.rm=T), date = date[1])%>%
+  group_by(day_in_year) %>% summarise(diff_ads = mean(total_ads - lag(total_ads), na.rm=T), date = date[2])%>%
   filter(!is.na(diff_ads)) %>% 
   ggplot() + 
   geom_line(aes(x=date, y=diff_ads))+theme_bw()+
-  labs(title="Difference in Cumulative Number of JOE Ads, 2019 vs. 2020 to Date")
+  labs(title="Difference in Cumulative Number of JOE Ads, 2019 vs. 2020 to Date",
+       x="Date", y="Cumulative YOY Change in Jobs on AEA")
 
-
+#Percent Change
+all_aea %>% 
+  filter(month%in% c(8)) %>%
+  arrange(day_in_year, year) %>%
+  group_by(day_in_year) %>% summarise(diff_ads = mean(total_ads - lag(total_ads), na.rm=T), date = date[2],
+                                      diff_ads_pct = mean((total_ads - lag(total_ads))/lag(total_ads), na.rm=T))%>%
+  filter(!is.na(diff_ads_pct)) %>% 
+  ggplot() + 
+  geom_line(aes(x=date, y=100*diff_ads_pct))+theme_bw()+
+  labs(title="Percent Change in Cumulative Number of JOE Ads, 2019 vs. 2020 to Date",
+       x="Date", y="Cumulative YOY Change in Jobs on AEA")
